1961ab7a7a2d057c42d93571fbb66576c6f2bd9d
Nessalyn <149561544+NessLM@users.noreply.github.com>
2025-08-17 18:20:35 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:trashvisor/pages/onboarding_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Created by Raihan Adi
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trashvisor App',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const WelcomePage(),
+    );
+  }
+}
